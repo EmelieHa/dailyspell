4d5e52156cecc78b006fda165017a912881ca0cc
EmelieHa <108530359+EmelieHa@users.noreply.github.com>
2023-02-28 12:49:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const urlToFetch = 'https://harry-potter-api-en.onrender.com/spells/';
+const urlToFetchTwo = 'https://harry-potter-api-en.onrender.com/characters/';
 
 let div = document.getElementById('spells');
-let spellName = document.querySelector('h2');
-let spellUse = document.querySelector('h3');
+let spellName = document.getElementById('spellName');
+let spellUse = document.getElementById('spellUse')
+let divTwo = document.getElementById('characters');
+let charName = document.getElementById('charsName');
+let charNick = document.getElementById('charsNick');
+
 
 function getSpell() {
     const endPoint = Math.floor(Math.random() * 72 + 1);
@@ -16,4 +21,17 @@ fetch(resultUrl)
 })
 };
 
+
+function getChar() {
+fetch(urlToFetchTwo)
+const endPoint = Math.floor(Math.random() * 23 + 1);
+const resultUrl = urlToFetchTwo + endPoint;
+fetch(resultUrl)
+.then((response) => response.json())
+.then((data) => {
+ charName.textContent = `Good morning! My name is ${data.character}, but you can call
+ me ${data.nickname}. I belong to ${data.hogwartsHouse}.\n Try one of my spells!`;
+
+})
+}
 
